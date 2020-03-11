6a9cffe6f96e9a04bb7a7b8c682da06d320ad006
Antoine <a.heidet90400@gmail.com>
2020-03-11 23:07:24 +0100

    ajout du template connexion + modification base SQl

---
+++ controller/connexion.php
@@ -0,0 +1,17 @@
+<?php
+
+function connexionView()
+{
+   include('views/helpdesk/connexion.php'); // inclure dans views front end listposts
+}
+function checkConnexion($username, $password)
+{
+
+    $adminManager = new adminManager();
+    if($adminManager->checkConnexion($username, $password) == 1 ){
+        $_SESSION['Logged'] = true;
+        header('Location: index.php?action=adminPanel');
+    }
+    return $adminManager->checkConnexion($username, $password);
+
+}
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 error_reporting(E_ALL); // reporting des erreur sur la page
 ini_set("display_errors", 1);
 require_once('controller/helpdesk.php');
+require_once('controller/connexion.php');
 /**if ($_GET['action'] == 'addTicket') {
         require_once('controller/backend.php');
         if (isset($_POST['numeroClient']) && isset($_POST['nom'])) { //SI l'action addpost et si on à du contenu dans titre et contenu
@@ -19,11 +20,31 @@ require_once('controller/helpdesk.php');
     }
 
 }**/
-if (isset($_GET['action'])) {
+/**if (isset($_GET['action'])) {
     if ($_GET['action'] == 'listTickets') {
         listTickets();
     }
+}**/
+
+if ($_GET['action'] == 'connexion') {
+    if(isset($_SESSION['Logged']) && $_SESSION['Logged'] =  true){
+        header('Location: index.php?action=adminPanel');
+     }
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        if (!empty($_POST['username']) && !empty($_POST['password'])) {
+            checkConnexion($_POST['username'],  $_POST['password']);
+        }
+        else {
+            echo 'Erreur : tous les champs ne sont pas remplis !';
+        }
+    }
+    connexionView();
 }
+if ($_GET['action'] == 'deconnexion') {
+    session_destroy();
+    header('Location: index.php?action=connexion');
+}
+
 
 /*require_once 'vendor/autoload.php';
 
