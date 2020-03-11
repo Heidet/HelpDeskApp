2c3372935bc1089a3173b27fbca45b07cdc1125a
Antoine <a.heidet90400@gmail.com>
2020-03-11 21:30:28 +0100

    mise en place template twig + test routage twig autoload.php

--- index.php
+++ index.php
@@ -25,4 +25,10 @@ if (isset($_GET['action'])) {
     }
 }
 
+/*require_once 'vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array('cache' => false)); 
+
+echo $twig->render('twig-home.html.twig');*/
 
