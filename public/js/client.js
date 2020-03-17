1af81d952870763912bcfc17c4b8ea9128b6b8cf
Antoine <a.heidet90400@gmail.com>
2020-03-17 21:15:09 +0100

    modification pour rajout fonctionnalité sur ticket client

---
+++ public/js/client.js
@@ -0,0 +1,26 @@
+// Exécute un appel AJAX GET
+// Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
+function ajaxGet(url, callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            // Appelle la fonction callback en lui passant la réponse de la requête
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    req.send(null);
+}
+/*ajaxGet("http://localhost/javascript-web-srv/data/films.json", function (reponse) {
+    // Transforme la réponse en tableau d'objets JavaScript
+    var films = JSON.parse(reponse);
+    // Affiche le titre de chaque film
+    films.forEach(function (film) {
+        console.log(film.titre);
+    })
+});*/
